70db12586c16949e6a8ddc6e265872637f3a64b7
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-02-27 21:41:52 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -20,44 +20,41 @@ public class Control {
     }
 
     public void turnOn() {
-        if (tv != null) {
             tv.turnOn();
-        }
+        
     }
 
     public void turnOff() {
-        if (tv != null) {
+
             tv.turnOff();
-        }
+
     }
 
     public void canalUp() {
-        if (tv != null) {
+
             tv.canalUp();
-        }
+
     }
 
     public void canalDown() {
-        if (tv != null) {
+
             tv.canalDown();
-        }
+
     }
     
     public void volumenUp() {
-    	if (tv != null) {
+
     		tv.volumenUp();
-    	}
+
     }
     
     public void volumendown() {
-    	if (tv != null) {
+
     		tv.volumenDown();
-    	}
     }
     
     public void setCanal(int canal) {
-        if (tv != null) {
+
             tv.setCanal(canal);
-        }
     }
 }
